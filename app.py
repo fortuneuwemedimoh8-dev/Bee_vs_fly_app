250be4b1e774ba557da2202228ede8039bc042e5
fortuneuwemedimoh8-dev <fortuneuwemedimoh8@gmail.com>
2026-07-26 04:23:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ if uploaded_file is not None:
     
     if st.button("Predict"):
         img = image.resize((150, 150))
-        img_array = np.array(img) / 255.0
+        img_array = np.array(img, dtype=np.float32)
+        
         img_array = np.expand_dims(img_array, axis=0)
 
         prediction = model.predict(img_array)[0][0]
